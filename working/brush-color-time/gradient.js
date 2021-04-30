803714d637f5ba85d58e26069782e69c630e6500
mmarol <mmarol01@risd.edu>
2021-04-30 15:09:25 -0400

    hopefully this works

--- working/brush-color-time/gradient.js
+++ working/brush-color-time/gradient.js
@@ -1,26 +1,25 @@
-var color1 = new Color(0.2, 0.64, 1.0, 0);
-var color2 = new Color(0.2, 0.64, 1.0, 1);
+var color1 = new Color(0.84, 0.64, 1.0, 0);
+var color2 = new Color(0.84, 0.64, 1.0, 1);
 // vec3(0.84, 0.64, 1.0)
 // hsl(273, 100%, 81%)
 
 var gradient, topLeft, bottomRight, gradientTween;
 
-rectFrom = view.bounds.leftCenter;
+rectFrom = view.bounds.leftCenter - new Point(0, 200);
 rectTo = view.bounds.bottomRight;
-gradientFrom = view.bounds.center;
-gradientTo = view.bounds.bottomCenter + new Point(0, 40);
+gradientFrom = view.bounds.center - new Point(0, 200);
+gradientTo = view.bounds.bottomCenter;
 gradient = new Shape.Rectangle({
 	from: rectFrom,
 	to: rectTo,
 	fillColor: {
 		gradient: {
-			stops: [[color1, 0.3], [color2, .8]],
+			stops: [[color1, 0], [color2, 1]],
 		},
 		origin: gradientFrom,
 		destination: gradientTo
 	}
 });
-console.log(gradient);
 
 gradientTween = gradient.tween({
 	opacity: 0
